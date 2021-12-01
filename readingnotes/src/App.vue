67648a47f9c4bd4b3c370e91828b7ed1732e3833
jancoyan <302848188@qq.com>
2021-12-01 22:36:25 +0800

    recreate project

--- readingnotes/src/App.vue
+++ readingnotes/src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
-    <router-view />
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .app{
+    background-color: #42b983;
+  }
+</style>
